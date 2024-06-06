a48ae6b9778ca6056a32806d62c2c49e25aaaf97
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-06 15:00:46 +0600

    counterSlice count add

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,22 +3,22 @@ import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
 
 export function Counter() {
-//   const count = useSelector((state) => state.counter.value)
-//   const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div>
       <div>
         <button
           aria-label="Increment value"
-        //   onClick={() => dispatch(increment())}
+          onClick={() => dispatch(increment())}
         >
           Increment
         </button>
         <span>{count}</span>
         <button
           aria-label="Decrement value"
-        //   onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(decrement())}
         >
           Decrement
         </button>
