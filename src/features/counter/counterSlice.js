dfbab369770fdf1bfc8c0cc53fbdc83a501781fd
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-09 13:04:59 +0600

    laravel commit

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,11 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
-}
+};
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   reducers: {
     increment: (state) => {
@@ -13,19 +13,21 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value ++
+      state.value++;
     },
 
     decrement: (state) => {
-      state.value --
+      if (state.value > 0) {
+        state.value--;
+      }
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.value += action.payload;
     },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
