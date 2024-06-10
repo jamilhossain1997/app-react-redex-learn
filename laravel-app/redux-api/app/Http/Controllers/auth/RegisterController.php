b758b334ef8baadd8b56d414e222b4d16c657392
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-10 12:18:53 +0600

    RegisterController-working

--- laravel-app/redux-api/app/Http/Controllers/auth/RegisterController.php
+++ laravel-app/redux-api/app/Http/Controllers/auth/RegisterController.php
@@ -14,36 +14,21 @@ use GuzzleHttp\Promise\Create;
 
 class RegisterController extends Controller
 {
-    public function UserRegister(Request $request){
-        $user=User::Create([
-           'name'=>$request->name,
-           'email'=>$request->email,
-           'password'=>Hash::make($request->password)
+    public function UserRegister(Request $request)
+    {
+        $user = User::Create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
 
-        $token=$user->createToken('Token')->accessToken ;
+        $token = $user->createToken('Token')->accessToken;
 
-      return  response()->json([
-          'status'=>200,
-          'user'=>$user,
-          'token'=>$token
+        return response()->json([
+            'status' => 200,
+            'user' => $user,
+            'token' => $token
         ]);
     }
-    public function UserLogin(Request $request){
-       $User=User::where('email',$request->email)->first();
-        $token=$User->createToken('Token')->accessToken;
-
-        if (!$User || !Hash::check($request->password,$User->password) ) {
-            return response([
-             "Error"=> "Not Match Email Or password"
-            ],401);
-         }
-          $token=$User->createToken('Token')->accessToken ;
-           return response([
-             'message'=>"successfull Login",
-              'user'=>$User,
-              'token'=>$token
-           ],200);
-      
-    }
+    
 }
