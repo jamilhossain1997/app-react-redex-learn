abfd08208fb1a1ee8c3b8332daf12edd60b341af
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-10 13:18:55 +0600

    api-route-working

--- laravel-app/redux-api/routes/api.php
+++ laravel-app/redux-api/routes/api.php
@@ -1,8 +1,7 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\auth\RegisterController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +14,21 @@ use App\Http\Controllers\auth\RegisterController;
 |
 */
 
-Route::post('UserRegister',[RegisterController::class,'UserRegister']);
-Route::post('UserLogin',[RegisterController::class,'UserLogin']);
+
+
+// auth
+Route::prefix('auth')->group(function () {
+    Route::post('UserRegister', 'App\Http\Controllers\Auth\RegisterController@UserRegister');
+    Route::post('UserLogin', 'App\Http\Controllers\Auth\LoginController@UserLogin');
+});
+
+
+//user 
+Route::prefix('user')->middleware('auth:api')->group(function () {
+    Route::get('UserView', 'App\Http\Controllers\Auth\userViewController@UserView');
+    Route::put('UserUpdate/{id}', 'App\Http\Controllers\Auth\userEditcontroller@UserUpdate');
+    Route::delete('UserDelect/{id}', 'App\Http\Controllers\Auth\userEditcontroller@UserDelect');
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
