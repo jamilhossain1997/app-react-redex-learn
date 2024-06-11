d9f5fcb5f343e60efe713f0a1d938e136c2e3362
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-11 15:50:21 +0600

    proViewController--

--- laravel-app/redux-api/app/Http/Controllers/product/proViewController.php
+++ laravel-app/redux-api/app/Http/Controllers/product/proViewController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class proViewController extends Controller
 {
-    //
+    public function ProductView(){
+       $product=Product::where('active',1)->get();
+
+       return response()->json([
+          "product"=>$product
+       ],200);
+    }
 }
