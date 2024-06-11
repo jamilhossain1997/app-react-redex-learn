558dc5bc05a082d36d9d1aa66a94e698ceaa2413
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-11 16:22:02 +0600

    proDelectController

--- laravel-app/redux-api/app/Http/Controllers/product/proDelectController.php
+++ laravel-app/redux-api/app/Http/Controllers/product/proDelectController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class proDelectController extends Controller
 {
-    //
+    public function ProductDelect(Request $request,$id){
+        $product=Product::findOrFail($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    
+        // Delete the product
+        $product->delete();
+    
+        return response()->json(['message' => 'Product deleted successfully']);
+    }
 }
