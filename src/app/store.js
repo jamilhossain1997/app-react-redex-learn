f2797abacdff73eed16155f5be601973db9312be
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-12 16:57:57 +0600

    storejs

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import counterReducer from '../features/counter/counterSlice';
+import apiSlice from '../service/apiSilce';
+import authReducer  from '../service/features/auth/authReducer';
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     counter: counterReducer,
+    auth: authReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
   },
 })
